198bfc70862531972cb3ef198ab88e3f8f6f1093
Dr Amr Osman <dr3mro@gmail.com>
2024-07-13 02:29:39 +0300

    update container

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,8 +6,8 @@ cd /usr/src/ProjectValhalla/
 git pull
 
 # Build the project
-cmake -S . -GNinja
-cmake --build . -j
+cmake -S . -Bbuild -GNinja
+cmake --build build -j
 
 # Execute the server
 exec ./server
