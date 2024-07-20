95bdd3737b5f34e3fb2ff1db7508ec089ff09c19
Dr. Amr Osman <dr3mro@gmail.com>
2024-07-20 11:31:20 +0300

    Switch from BCrypt to libsodium scrypt for better performance (10x).

---
+++ src/utils/passwordcrypt.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <optional>
+#include <string>
+
+class PasswordCrypt {
+public:
+    PasswordCrypt();
+    ~PasswordCrypt() = default;
+
+    std::optional<std::string> hashPassword(const std::string& password);
+    bool verifyPassword(const std::string& password, const std::string& hash) const;
+};
