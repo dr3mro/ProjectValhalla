02687d9ff46c61e6f36ed659c227337f382615ee
Dr. Amr Osman <dr3mro@gmail.com>
2024-07-21 16:26:26 +0300

    Add missing units.

---
+++ src/utils/getvars.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <optional>
+#include <string>
+
+class EnvVars {
+public:
+    // Method to get an environment variable by name
+    std::optional<std::string> get(const std::string& key) const;
+
+    // Method to set an environment variable
+    void set(const std::string& key, const std::string& value);
+};
