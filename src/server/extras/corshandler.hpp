8543418fe11734202077f49c2b09c25876bee8c4
Dr. Amr Osman <dr3mro@gmail.com>
2024-07-24 13:15:55 +0300

    Test before release

--- src/server/extras/corshandler.hpp
+++ src/server/extras/corshandler.hpp
@@ -12,32 +12,32 @@ public:
 
         // Global CORS settings
         cors.global()
-            .origin("http://localhost:3000")
-            .methods("GET"_method, "OPTIONS"_method)
+            .origin("*")
+            .methods("GET"_method)
             .headers("Content-Type", "Accept-Encoding", "Origin")
             .max_age(3600)
             .allow_credentials();
 
         // CORS settings for /v1/patient prefix
         cors.prefix("/v1/patient")
-            .origin("http://localhost:3000")
-            .methods("GET"_method, "POST"_method, "DELETE"_method, "PATCH"_method, "SEARCH"_method, "OPTIONS"_method)
+            .origin("*")
+            .methods("GET"_method, "POST"_method, "DELETE"_method, "PATCH"_method, "SEARCH"_method)
             .headers("Content-Type", "Accept-Encoding", "Origin", "Authorization")
             .max_age(7200)
             .allow_credentials();
 
         // CORS settings for /v1/user prefix
         cors.prefix("/v1/user")
-            .origin("http://localhost:3000")
-            .methods("GET"_method, "POST"_method, "OPTIONS"_method)
+            .origin("*")
+            .methods("GET"_method, "POST"_method)
             .headers("Content-Type", "Accept-Encoding", "Origin", "Authentication")
             .max_age(7200)
             .allow_credentials();
 
         // CORS settings for /v1/hello prefix
         cors.prefix("/v1/hello")
-            .origin("http://localhost:3000")
-            .methods("GET"_method, "OPTIONS"_method)
+            .origin("*")
+            .methods("GET"_method)
             .headers("Content-Type", "Accept-Encoding", "Origin")
             .max_age(7200)
             .allow_credentials();
